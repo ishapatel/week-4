9d82f886a46b208fefc2608c1bb95ea094651bc9
ishapatel <isha1992@gmail.com>
2015-10-14 18:00:26 -0400

    ip2286_week4.2

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def getData():
 	return json.dumps(output)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
+    app.run(host='0.0.0.0',port=5001,debug=True,threaded=True)
